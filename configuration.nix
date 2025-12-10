cced541cb8f803137965ac2e2ff62d2e9839d38a
Jude Fanner <73987359+UR91K@users.noreply.github.com>
2025-12-10 20:27:39 +0200

    Improve graphics setup
    Updated comments for clarity and added enhanced graphics configuration for VirtualBox compatibility.

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,6 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# and in the NixOS manual (accessible by running 'nixos-help').
 
 { config, pkgs, ... }:
 
@@ -46,7 +46,6 @@
   virtualisation.virtualbox.guest.enable = true;
   virtualisation.virtualbox.guest.dragAndDrop = true;
 
-
   environment.systemPackages = with pkgs; [
     neovim
     kitty
@@ -55,7 +54,7 @@
     git
   ];
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Define a user account. Don't forget to set a password with 'passwd'.
   users.users.judef = {
     isNormalUser = true;
     description = "Jude Fanner";
@@ -68,9 +67,21 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
   
-  hardware.graphics.enable = true;
+  # Enhanced graphics configuration for VirtualBox
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      mesa
+      libGL
+    ];
+  };
+  
+  # Force software rendering for VirtualBox compatibility
   environment.sessionVariables = {
     WGPU_BACKEND = "gl";
+    WLR_RENDERER = "pixman";
+    WLR_NO_HARDWARE_CURSORS = "1";
+    LIBGL_ALWAYS_SOFTWARE = "1";
   };
 
   environment.etc."xdg/niri/config.kdl".text = ''
@@ -114,7 +125,7 @@
     
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # on your system were taken. It's perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
